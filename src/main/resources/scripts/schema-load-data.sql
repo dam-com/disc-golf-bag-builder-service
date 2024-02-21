3c6591ae6b268f745e97024f0c0ce62a7bec036d
Dustin Klein <dbklein4@gmail.com>
2024-02-21 15:00:17 -0600

    fix load schema sql

--- src/main/resources/scripts/schema-load-data.sql
+++ src/main/resources/scripts/schema-load-data.sql
@@ -1,11 +1,22 @@
-SET search_path TO public;
-CREATE TABLE public.user
+set schema 'public';
+
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
+
+CREATE TABLE "user"
 (
     id BIGINT NOT NULL,
     name CHARACTER VARYING(255) NOT NULL
 );
-ALTER TABLE public.user ADD PRIMARY KEY (id);
-CREATE TABLE public.main_disc
+ALTER TABLE "user" ADD PRIMARY KEY (id);
+
+
+CREATE TABLE main_disc
 (
     id BIGINT NOT NULL,
     name CHARACTER VARYING(255) NOT NULL,
@@ -25,22 +36,28 @@ CREATE TABLE public.main_disc
     rim_configuration CHARACTER VARYING(255) NULL,
     flexibility CHARACTER VARYING(255) NULL
 );
-ALTER TABLE public.main_disc ADD PRIMARY KEY (id);
-CREATE TABLE public.manufacturer
+ALTER TABLE main_disc ADD PRIMARY KEY (id);
+
+
+CREATE TABLE manufacturer
 (
     id BIGINT NOT NULL,
     name BIGINT NULL
 );
-ALTER TABLE public.manufacturer ADD PRIMARY KEY (id);
-CREATE TABLE public.bag
+ALTER TABLE manufacturer ADD PRIMARY KEY (id);
+
+
+CREATE TABLE bag
 (
     id BIGINT NOT NULL,
     user_id BIGINT NULL,
     name CHARACTER VARYING(255) NULL,
     type CHARACTER VARYING(255) NULL
 );
-ALTER TABLE public.bag ADD PRIMARY KEY (id);
-CREATE TABLE public.disc_bag
+ALTER TABLE bag ADD PRIMARY KEY (id);
+
+
+CREATE TABLE disc_bag
 (
     id BIGINT NOT NULL,
     bag_id BIGINT NOT NULL,
@@ -48,8 +65,10 @@ CREATE TABLE public.disc_bag
     user_id BIGINT NOT NULL,
     favorite BOOLEAN NULL
 );
-ALTER TABLE public.disc_bag ADD PRIMARY KEY (id);
-CREATE TABLE public.plastic
+ALTER TABLE disc_bag ADD PRIMARY KEY (id);
+
+
+CREATE TABLE plastic
 (
     id BIGINT NOT NULL,
     manufacturer_id BIGINT NOT NULL,
@@ -57,16 +76,19 @@ CREATE TABLE public.plastic
     description CHARACTER VARYING(255) NULL,
     classification CHARACTER VARYING(255) NULL
 );
-ALTER TABLE public.plastic ADD PRIMARY KEY (id);
---COMMENT ON COLUMN plastic.classification IS 'base, premium?';
-CREATE TABLE public.shelf
+ALTER TABLE plastic ADD PRIMARY KEY (id);
+
+
+CREATE TABLE shelf
 (
     id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     disc_id BIGINT NOT NULL
 );
-ALTER TABLE public.shelf ADD PRIMARY KEY (id);
-CREATE TABLE public.disc
+ALTER TABLE shelf ADD PRIMARY KEY (id);
+
+
+CREATE TABLE disc
 (
     id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
@@ -83,40 +105,15 @@ CREATE TABLE public.disc
     color CHARACTER VARYING(255) NULL,
     favorite BOOLEAN NULL
 );
--- ALTER TABLE public.disc ADD PRIMARY KEY (id);
--- ALTER TABLE public.disc_bag
--- ADD CONSTRAINT disc_bag_user_id_foreign
---     FOREIGN KEY (user_id)
---     REFERENCES public.user (id);
--- ALTER TABLE public.plastic
--- ADD CONSTRAINT plastic_manufacturer_id_foreign
---     FOREIGN KEY (manufacturer_id)
---     REFERENCES public.manufacturer (id);
--- ALTER TABLE public.main_disc
--- -- ADD CONSTRAINT main_disc_manufacturer_foreign
--- --     FOREIGN KEY (manufacturer)
--- --     REFERENCES public.manufacturer (id);
--- -- ALTER TABLE public.disc
--- ADD CONSTRAINT disc_user_id_foreign
---     FOREIGN KEY (user_id)
---     REFERENCES public.user (id);
--- ALTER TABLE public.shelf
--- ADD CONSTRAINT shelf_user_id_foreign
---     FOREIGN KEY (user_id)
---     REFERENCES public.user (id);
--- ALTER TABLE public.bag
--- ADD CONSTRAINT bag_user_id_foreign
---     FOREIGN KEY (user_id)
---     REFERENCES public.user (id);
--- ALTER TABLE public.disc_bag
--- ADD CONSTRAINT disc_bag_disc_id_foreign
---     FOREIGN KEY (disc_id)
---     REFERENCES public.disc (id);
--- ALTER TABLE public.disc_bag
--- ADD CONSTRAINT disc_bag_bag_id_foreign
---     FOREIGN KEY (bag_id)
---     REFERENCES public.bag (id);
--- ALTER TABLE public.shelf
--- ADD CONSTRAINT shelf_disc_id_foreign
---     FOREIGN KEY (disc_id)
---     REFERENCES public.disc (id);
+ALTER TABLE disc ADD PRIMARY KEY (id);
+
+
+ALTER TABLE disc_bag ADD CONSTRAINT disc_bag_user_id_foreign FOREIGN KEY(user_id) REFERENCES "user"(id);
+ALTER TABLE plastic ADD CONSTRAINT plastic_manufacturer_id_foreign FOREIGN KEY(manufacturer_id) REFERENCES manufacturer(id);
+--ALTER TABLE main_disc ADD CONSTRAINT main_disc_manufacturer_foreign FOREIGN KEY(manufacturer) REFERENCES manufacturer(id);
+ALTER TABLE disc ADD CONSTRAINT disc_user_id_foreign FOREIGN KEY(user_id) REFERENCES "user"(id);
+ALTER TABLE shelf ADD CONSTRAINT shelf_user_id_foreign FOREIGN KEY(user_id) REFERENCES "user"(id);
+ALTER TABLE bag ADD CONSTRAINT bag_user_id_foreign FOREIGN KEY(user_id) REFERENCES "user"(id);
+ALTER TABLE disc_bag ADD CONSTRAINT disc_bag_disc_id_foreign FOREIGN KEY(disc_id) REFERENCES disc(id);
+ALTER TABLE disc_bag ADD CONSTRAINT disc_bag_bag_id_foreign FOREIGN KEY(bag_id) REFERENCES bag(id);
+ALTER TABLE shelf ADD CONSTRAINT shelf_disc_id_foreign FOREIGN KEY(disc_id) REFERENCES disc(id);

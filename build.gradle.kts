c898edb39603664e754d0c949fada8daf700e259
Abby Klein <aklein.tech@gmail.com>
2024-02-20 20:03:38 -0600

    Add schema.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
     implementation("io.quarkus:quarkus-container-image-docker")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))

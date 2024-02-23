5a0177352d6c4e82793fbd113525dffef50c695e
Dustin Klein <dbklein4@gmail.com>
2024-02-23 15:02:24 -0600

    add sequence

--- src/main/kotlin/com/dam/model/Bag.kt
+++ src/main/kotlin/com/dam/model/Bag.kt
@@ -1,17 +1,15 @@
 package com.dam.model
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "bag")
 class Bag {
     @Id
+    @GeneratedValue(generator = "bag_id_seq", strategy = GenerationType.SEQUENCE)
     var id: Long? = null
     @Column(name = "user_id")
-    val userId: Long? = null
+    var userId: Long? = null
     lateinit var name: String
     lateinit var type: String
 }
